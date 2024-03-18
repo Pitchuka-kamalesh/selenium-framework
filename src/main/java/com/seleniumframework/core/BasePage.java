7f3f542f83c4d629f6c67c9b53846882bcbf2be0
Pitchuka-kamalesh <kamalesh.4025@gmail.com>
2024-03-18 19:31:25 +0530

    Added the sendKeys,ClearKeys,safeClick methods in the BasePage.java

--- src/main/java/com/seleniumframework/core/BasePage.java
+++ src/main/java/com/seleniumframework/core/BasePage.java
@@ -1,26 +1,41 @@
 package com.seleniumframework.core;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
-    WebDriver driver;
+    private static final WebDriver driver = DriverManager.getDriver();
 
 
     // i must implements all the selenium common functions here.
-    public BasePage(){
-        PageFactory.initElements(DriverManager.getDriver(),this);
+    public BasePage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public static void sendKeys(WebElement element,int timeout,String value){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+        wait.until(ExpectedConditions.elementToBeSelected(element));
+        element.sendKeys(value);
+    }
+
+    public static void clearKeys(WebElement element,int timeout){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+        wait.until(ExpectedConditions.elementToBeSelected(element));
+        element.clear();
+
+    }
+    public static void safeClick(WebElement element,int timeout){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(timeout));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
     }
 
 
-    /*
-    sendKeys
-    clear
-    wait methods
-     select method
-     screenshot
-     js
-    Boarders
 
-    */
+
 }
