724a873aeb61ed27da650917788feb4c20a0d771
Pitchuka-kamalesh <kamalesh.4025@gmail.com>
2024-03-22 14:41:49 +0530

    removed un used  imports

--- src/main/java/com/seleniumframework/core/TestListener.java
+++ src/main/java/com/seleniumframework/core/TestListener.java
@@ -2,10 +2,8 @@ package com.seleniumframework.core;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.MediaEntityBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.support.ThreadGuard;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
